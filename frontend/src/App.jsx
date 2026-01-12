b02a22894755f827068d0db58fed07f856b84f53
Nikhil <nikhil.medepalli27@gmail.com>
2026-01-12 16:21:18 +0530

    frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar"
+import { Routes, Route } from "react-router"
+import Home from "./pages/Home"
+import Product from "./pages/Product"
+import Profile from "./pages/Profile"
+import Create from "./pages/Create"
+import EditProduct from "./pages/EditProduct"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <div className="min-h-screen bg-base-100">
+      <Navbar />
+      <main className="max-w-5xl mx-auto px-4 py-8">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/edit/:id" element={<EditProduct />} />
+        </Routes>
+      </main>
+   </div>
   )
 }
-
 export default App
